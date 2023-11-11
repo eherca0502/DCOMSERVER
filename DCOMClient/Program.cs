023bbc4527436b0831b9f05d2b7a00a59b8c1cc6
eduar <eduar@DESKTOP-CQ4EC7K>
2023-11-10 20:11:58 -0600

    Agregar archivos de proyecto.

---
+++ DCOMClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DCOMSERVER;
+
+
+namespace DCOMClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce un numero");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            double sumar = Operaciones.Sumar(numero1, numero2);
+            double restar = Operaciones.Restar(numero1, numero2);
+            Console.WriteLine("Resultado de Suma : " + sumar);
+            Console.WriteLine("Resultado de Resta : " + restar);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DCOMSERVER/Operaciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCOMSERVER
+{
+    public class Operaciones
+    {
+        public static double Sumar(double numero1,double numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public static double Restar(double numero1, double numero2)
+        {
+            return numero1 - numero2;
+        }
+    }
+}
